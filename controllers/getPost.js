f4fd24e103e6bce345e5d9ccfe69b1338c951684
siddhartha <gsiddharth123@gmail.com>
2021-04-30 21:10:20 +0530

    fourth

--- controllers/getPost.js
+++ controllers/getPost.js
@@ -1,7 +1,7 @@
 const Post = require('../database/models/Post')
  
 module.exports = async (req, res) => {
-    const post = await Post.findById(mongoose.Types.ObjectId);    
+    const post = await Post.findById(req.params.id);         
     res.render("post", {
         post
     });
